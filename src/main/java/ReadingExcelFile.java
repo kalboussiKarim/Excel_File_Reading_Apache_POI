72cf085929131e6d32376c7564eb3747584595f2
kalboussi karim <kalboussikarim3@gmail.com>
2025-07-04 10:42:51 +0100

    tabulation enhancement

--- src/main/java/ReadingExcelFile.java
+++ src/main/java/ReadingExcelFile.java
@@ -36,25 +36,17 @@ public class ReadingExcelFile {
     private static String getCellValue(Cell cell) {
         if (cell == null) return "";
         switch (cell.getCellType()) {
-            case STRING:
-                return cell.getStringCellValue();
-            case NUMERIC:
-                return String.valueOf(cell.getNumericCellValue());
-            case BOOLEAN:
-                return String.valueOf(cell.getBooleanCellValue());
+            case STRING: return cell.getStringCellValue();
+            case NUMERIC: return String.valueOf(cell.getNumericCellValue());
+            case BOOLEAN: return String.valueOf(cell.getBooleanCellValue());
             case FORMULA:
                 switch (cell.getCachedFormulaResultType()) {
-                    case STRING:
-                        return cell.getStringCellValue();
-                    case NUMERIC:
-                        return String.valueOf(cell.getNumericCellValue());
-                    case BOOLEAN:
-                        return String.valueOf(cell.getBooleanCellValue());
-                    default:
-                        return "";
+                    case STRING: return cell.getStringCellValue();
+                    case NUMERIC: return String.valueOf(cell.getNumericCellValue());
+                    case BOOLEAN: return String.valueOf(cell.getBooleanCellValue());
+                    default: return "";
                 }
-            default:
-                return "";
+            default: return "";
         }
     }
 }
